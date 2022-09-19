5ac5170c78ed8af0b6de940e0dc89fde620c38bd
Marcelo Guerra <marcelovgl99@hotmail.com>
2022-09-19 19:32:04 -0300

    teste

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.delete('/movie/:id', (req,res) => {
     res.send("FIlme foi apagado!");
 });
 
-app.listen(8080, () => console.log('Servidor iniciado na porta 8080'));
+app.listen(3000, () => console.log('Servidor iniciado na porta 3000'));
 
 
